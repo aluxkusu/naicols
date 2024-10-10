83c895ba45b63c790ec7961d832776d3d2b66974
aluxkusu <aluxkusu@gmail.com>
2024-10-10 23:09:55 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function sorpresa(){
+	document.getElementById('imagen').src ='https://imgs.search.brave.com/bOyltLBDQBNyA32uoR0ediFO3UjZm8_xE73fcZBx4aY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2xhcmluLmNvbS9p/bWcvMjAyMC8wNS8y/OS82OF9vd3FNNnFf/MTI1Nng2MjBfXzIu/anBnIzE1OTA3Nzkx/MTc5ODg';
+	alert("PARA NICOLE LA DEL DÍA 10/10");
+}
